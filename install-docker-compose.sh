16ba4b284c7ab120d5430564789d4b53b3dba8ca
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2023-05-27 23:40:26 +0200

    update sh: upgrade docker-compose to v2.18.1

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,4 +1,5 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
